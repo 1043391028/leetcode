1eeac8156484bc4fd36a59aa905e83ecd1d23bc5
gwzstar <guanweizheng66@163.com>
2020-11-17 23:59:16 +0800

    leetcode更新20.11.17

---
+++ src/leetcode_00BinaryTree/_94_inOrderSearchTree/Solution1.java
@@ -0,0 +1,37 @@
+package leetcode_00BinaryTree._94_inOrderSearchTree;
+
+//     题目：给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+
+//    思路1： 常规的中序遍历；简单好理解；
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution1 {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+//    设置全局链表变量，保存中序遍历函数遍历过程的结果；
+    List<Integer> res = new ArrayList<Integer>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root == null) return res;
+        help(root);
+        return res;
+    }
+//  中序遍历辅助函数；
+    public void help(TreeNode root){
+        if(root == null) return ;
+        help(root.left);
+        res.add(root.val);
+        help(root.right);
+    }
+}
---
+++ src/leetcode_00BinaryTree/_96_differentBinaryTree/Solution1.java
@@ -0,0 +1,29 @@
+package leetcode_00BinaryTree._96_differentBinaryTree;
+
+//      题目：给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+//            输入: 3
+//            输出: 5
+//      解释:
+//           给定 n = 3, 一共有 5 种不同结构的二叉搜索树:
+//
+//           1         3     3      2      1
+//            \       /     /      / \      \
+//             3     2     1      1   3      2
+//            /     /       \                 \
+//            2     1         2                 3
+//
+//      思路： dp
+
+public class Solution1 {
+    public int numTree(int n){
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2;i <= n;i++){
+            for(int j = 0;j < i;j++){
+                dp[i] += dp[j] * dp[i-j-1];
+            }
+        }
+        return dp[n];
+    }
+}
