bafc87d08e45961eaf9f0de0a4857d949f26d596
gwzstar <guanweizheng66@163.com>
2020-11-23 22:53:30 +0800

    leetcode更新20.11.23

--- src/leetcode_136_findNumOnlyOneTime/Solution1.java
+++ src/leetcode_136_findNumOnlyOneTime/Solution1.java
@@ -4,7 +4,7 @@ package leetcode_136_findNumOnlyOneTime;
 //
 //   说明： 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
 //
-//   思路： 利用位运算 ‘异或’ 特性
+//   思路： 利用位运算 ‘异或’ 特性（两个相同的数异或结果为 0，任何数与 0 异或还是它本身）
 public class Solution1 {
     public int singleNumber(int[] nums) {
         int num = nums[0];
