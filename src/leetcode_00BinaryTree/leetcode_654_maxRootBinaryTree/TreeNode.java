1bb978af75be9f0fcb8fbbc28c5f882927d9b1f4
gwzstar <guanweizheng66@163.com>
2020-11-24 23:24:11 +0800

    leetcode更新20.11.24

---
+++ src/leetcode_00BinaryTree/leetcode_654_maxRootBinaryTree/TreeNode.java
@@ -0,0 +1,9 @@
+package leetcode_00BinaryTree.leetcode_654_maxRootBinaryTree;
+
+public class TreeNode {
+
+    int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+}
