1f004abed44eaaf8ccd7f3e5923d92493fa5f56b
gwzstar <guanweizheng66@163.com>
2020-11-26 12:04:19 +0800

    leetcode_2020.11.26

---
+++ src/leetcode_141_circleList1/ListNode.java
@@ -0,0 +1,10 @@
+package leetcode_141_circleList1;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
---
+++ src/leetcode_142_circleList2/ListNode.java
@@ -0,0 +1,10 @@
+package leetcode_142_circleList2;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
