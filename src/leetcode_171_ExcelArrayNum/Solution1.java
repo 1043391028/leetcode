96b736986e01627723b14b50b32c07fb68497026
gwzstar <guanweizheng66@163.com>
2021-01-12 22:20:46 +0800

    leetcode_2021.01.12

---
+++ src/leetcode_171_ExcelArrayNum/Solution1.java
@@ -0,0 +1,36 @@
+package leetcode_171_ExcelArrayNum;
+
+//  题目：excel表格序号；
+//  描述： 给定一个Excel表格中的列名称，返回其相应的列序号。
+//
+//  例如，
+//       A -> 1
+//       B -> 2
+//       C -> 3
+//        ...
+//       Z -> 26
+//       AA -> 27
+//       AB -> 28
+//       ...
+//
+//  思路一： 新建一个大小为26的数组，分别从小到大存1-26（对应A-Z）
+//             遍历 s 字符串，每个字符 ch-'A'可表示下标；
+
+public class Solution1 {
+    public int titleToNumber(String s) {
+        // 新建数组，保存对应数 1 - 26；
+        int[] nums = new int[26];
+        for(int i =0;i<26;i++){
+            nums[i] = i+1;
+        }
+        // 遍历字符串每个字符，并计算结果；
+        //   字符对应的 数组下标为 s.charAt(i) - 'A'
+        int len = s.length();
+        int sum = 0;
+        for(int i = 0;i<len;i++){
+            int temp = nums[s.charAt(i)-'A'];
+            sum = sum*26 + temp;
+        }
+        return sum;
+    }
+}
