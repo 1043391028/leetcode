f14d8e15c9a939f48d38b9c9689d726f7e3f1984
gwzstar <guanweizheng66@163.com>
2021-04-11 14:34:06 +0800

    nk_练习_2021.04.11

---
+++ jianzhiOffer/src/Face_test/_001_MostPopular/_007_LRU/LRUCache.java
@@ -0,0 +1,39 @@
+package Face_test._001_MostPopular._007_LRU;
+
+//  题目： Lru 缓存机制；
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache {
+    private int cap;
+    private HashMap<Integer,Integer> map;
+
+    public LRUCache(int capacity) {
+        this.cap = capacity;
+        map = new LinkedHashMap<Integer,Integer>();
+    }
+
+    public int get(int key) {
+        if(map.containsKey(key)){
+            int value = map.get(key);
+            map.remove(key);
+            map.put(key,value);
+            return value;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        if(map.containsKey(key)){
+            map.remove(key);
+        }else if(map.size() == cap){
+            Iterator<Map.Entry<Integer,Integer>> it = map.entrySet().iterator();
+            it.next();
+            it.remove();
+        }
+        map.put(key,value);
+    }
+}
